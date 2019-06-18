8d15d28bf9cb57cd07c86a2ae1a09c7978ca66c7
PowerPollery <34681639+PowerPollery@users.noreply.github.com>
2019-06-18 11:50:08 +0800

    Update About.java

--- src/view/About.java
+++ src/view/About.java
@@ -33,7 +33,7 @@ public class About extends JDialog{
         ta.setFont(new Font("微软雅黑", 0, 15));
         ta.setEnabled(false);
         ta.setLineWrap(true);
-        ta.setText("版本：记事本V2.3\n\n作者：郑云瑞\n开发语言：Java\n开发环境：IntelliJ IDEA\n更新时间：2019-6-8\n\n简易记事本，记录每一天！\n- 加入了完美的高亮功能，记笔记写代码不再疲劳！\n- 打开方式优化。\n- 加入了代码模式，优化高亮优先级。\n- 修复若干BUG。");
+        ta.setText("版本：记事本V2.3 (UTF8)\n\n作者：郑云瑞\n开发语言：Java\n开发环境：IntelliJ IDEA\n更新时间：2019-6-8\n\n简易记事本，记录每一天！\n- 加入了完美的高亮功能，记笔记写代码不再疲劳！\n- 打开方式优化。\n- 加入了代码模式，优化高亮优先级。\n- 修复若干BUG。");
         JButton button = new JButton("确定");
         button.setBounds(380, 400, 80, 30);
         button.addActionListener(new ActionListener() {
